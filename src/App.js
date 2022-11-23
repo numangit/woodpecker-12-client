8fbeb919c673ad230d4b66579caa400ac5509625
numangit <abdulnumanjob@gmail.com>
2022-11-24 01:06:23 +0600

    basic router setup added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Routes/Routes/Routes';
 
 function App() {
   return (
     <div className="bg-[#fefcf7]">
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/SharedComponents/Footer/Footer';
+import Navbar from '../Pages/SharedComponents/Navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
-
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
