784363b91378eab363c67385a9fe636ef4361237
numangit <abdulnumanjob@gmail.com>
2022-11-28 09:25:32 +0600

    styling adjustments added

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Woodpecker is a full-stack website using React, firebase, react router, node, ex
 Woodpecker is a secondhand furniture resale website. Color pallets used on this website are comforting to eyes and user experience is not compromised. Smooth animation included where needed. The website includes payment system, Admin dashboard, admin and buyer data customization and many more.
 
 ## Live Site
-Hosted on Firebase -> [Woodpecker](https://woodpecker-12.firebaseapp.com)
+Hosted on Firebase -> [Woodpecker](https://woodpecker-12.web.app/)
 
 ## Project Features
 
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div className="hero min-h-screen" style={{ backgroundImage: `url("//cdn.shopify.com/s/files/1/0077/2970/9137/files/slider-img_4.png?v=1613714145")` }}>
             <div className=" bg-opacity-0"></div>
-            <div className="hero-content ml-auto mr-20 text-center text-gray-900">
+            <div className="hero-content lg:ml-auto lg:mr-20 text-center text-gray-900">
                 <div className="max-w-md">
                     <h1 data-aos="fade-down" className="my-1 text-5xl lg:text-6xl font-bold">Woodpecker</h1>
                     <p data-aos="fade-left" data-aos-duration="800" className="ml-auto text-xl lg:text-2xl font-thin">The Best Quality Used Furniture</p>
