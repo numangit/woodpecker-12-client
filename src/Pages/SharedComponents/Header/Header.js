47b0fb6dc9dfd45e1af1c4bb9f33894120261611
numangit <abdulnumanjob@gmail.com>
2023-04-20 04:55:36 +0600

    close button added to nav header

--- src/Pages/SharedComponents/Header/Header.js
+++ src/Pages/SharedComponents/Header/Header.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Header = () => {
+    const [isClosed, setIsClosed] = useState(false);
     return (
-        <div className='bg-gradient-to-r from-violet-900 to-cyan-700'>
-            <p className='text-white text-center'>Get 20% discount on our advertised products, shop now🎉</p>
+        <div className={`bg-gradient-to-r from-violet-900 to-cyan-700 flex ${isClosed && "hidden"}`}>
+            <p className='text-white text-center flex-1'>Get 20% discount on our advertised products, shop now🎉</p>
+<button onClick={()=>setIsClosed(true)} className='mr-5 font-bold'>x</button>
         </div>
     );
 };
