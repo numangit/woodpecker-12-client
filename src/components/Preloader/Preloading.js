4588860a1598106e6cd595b5a1c38e89c8c27ab8
numangit <abdulnumanjob@gmail.com>
2023-04-20 02:44:40 +0600

    preloader changed

--- src/components/Preloader/Preloading.js
+++ src/components/Preloader/Preloading.js
@@ -1,11 +1,11 @@
 import Lottie from 'lottie-react';
 import React from 'react';
-import preloader from '../../assets/9606-wave-loader.json';
+import preloader from '../../assets/9582-liquid-4-dot-loader.json';
 
 const Preloading = () => {
     return (
         <div className='h-screen bg-slate-900 flex justify-center items-center'>
-        <div className='w-1/5'>
+        <div className='w-1/3'>
             <Lottie animationData={preloader} loop={true} />
         </div>
     </div>
