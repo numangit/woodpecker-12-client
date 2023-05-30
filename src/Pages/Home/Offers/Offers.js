c3c422460a4dd56d0e104dc97386345edb1cb6e6
numangit <abdulnumanjob@gmail.com>
2023-05-30 21:15:37 +0600

    offers formatted

--- src/Pages/Home/Offers/Offers.js
+++ src/Pages/Home/Offers/Offers.js
@@ -4,7 +4,7 @@ const Offers = () => {
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row items-center justify-between">
           <h2 className="text-center text-6xl tracking-tighter font-bold">Up to
-            <br className="sm:hidden" />50% Off
+            <br className="sm:hidden" />&nbsp;50% Off
           </h2>
           <div className="space-x-2 text-center py-2 lg:py-0">
             <span>Plus free shipping! Use code:</span>
