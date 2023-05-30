1b4b8af43878c84095ed21cdc78c39292d280631
numangit <abdulnumanjob@gmail.com>
2023-05-30 22:29:07 +0600

    testimonial quote added

--- src/Pages/Home/Testimonial/Testimonial.js
+++ src/Pages/Home/Testimonial/Testimonial.js
@@ -1,44 +1,30 @@
-import React from 'react';
+import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
 
 const Testimonial = () => {
   return (
-    <section className="my-8 mx-2 md:mx-0">
+    <section className="mx-2 md:mx-0 p-8 bg-fixed bg-cover text-white" style={{ backgroundImage: `url("https://images.unsplash.com/photo-1512747646639-ed824d861e0d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80")` }}>
       <div className="container mx-auto flex flex-col items-center pb-6 mb-4 md:p-10 md:px-12">
         <h1 className="text-3xl font-semibold leading-none text-center">What our customers are saying about us</h1>
       </div>
       <div className="container mx-auto grid grid-cols-1 gap-8 lg:gap-20 md:px-10 md:pb-10 lg:grid-cols-2">
         <div className="flex flex-col items-center mx-12 lg:mx-0">
           <div className="relative text-center">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute top-0 left-0 w-8 h-8 dark:text-gray-700">
-              <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
-              <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-            </svg>
+            <FaQuoteLeft />
             <p className="px-6 py-1 text-lg italic">I was so disappointed with my new coffee table. It arrived damaged, and the customer service was terrible. I would not recommend this company to anyone.</p>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute bottom-0 right-0 w-8 h-8 dark:text-gray-700">
-              <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
-              <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
-            </svg>
           </div>
           <span className="w-12 h-1 my-2 rounded-lg dark:bg-violet-400"></span>
           <p>Leroy Canon</p>
         </div>
         <div className="flex flex-col items-center max-w-lg mx-12 lg:mx-0">
           <div className="relative text-center">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="absolute top-0 left-0 w-8 h-8 dark:text-gray-700">
-              <path fill="currentColor" d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
-              <path fill="currentColor" d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-            </svg>
+            <FaQuoteLeft />
             <p className="px-6 py-1 text-lg italic">I love my new sofa! It's so comfortable and stylish, and it arrived in perfect condition. I would definitely recommend this company to anyone looking for new furniture.</p>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="absolute bottom-0 right-0 w-8 h-8 dark:text-gray-700">
-              <path fill="currentColor" d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
-              <path fill="currentColor" d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
-            </svg>
           </div>
           <span className="w-12 h-1 my-2 rounded-lg dark:bg-violet-400"></span>
           <p>Riyad Ahmed</p>
         </div>
       </div>
-    </section>
+    </section >
   );
 };
 
