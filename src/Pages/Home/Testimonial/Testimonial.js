58c1750791eb778473d03cffd2b44015593b1bc7
numangit <abdulnumanjob@gmail.com>
2023-05-30 21:35:10 +0600

    testimonial comments updated

--- src/Pages/Home/Testimonial/Testimonial.js
+++ src/Pages/Home/Testimonial/Testimonial.js
@@ -13,14 +13,14 @@ const Testimonial = () => {
               <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
               <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
             </svg>
-            <p className="px-6 py-1 text-lg italic">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus quibusdam, eligendi exercitationem molestias possimus facere.</p>
+            <p className="px-6 py-1 text-lg italic">I was so disappointed with my new coffee table. It arrived damaged, and the customer service was terrible. I would not recommend this company to anyone.</p>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute bottom-0 right-0 w-8 h-8 dark:text-gray-700">
               <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
               <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
             </svg>
           </div>
           <span className="w-12 h-1 my-2 rounded-lg dark:bg-violet-400"></span>
-          <p>Leroy Jenkins</p>
+          <p>Leroy Canon</p>
         </div>
         <div className="flex flex-col items-center max-w-lg mx-12 lg:mx-0">
           <div className="relative text-center">
@@ -28,14 +28,14 @@ const Testimonial = () => {
               <path fill="currentColor" d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
               <path fill="currentColor" d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
             </svg>
-            <p className="px-6 py-1 text-lg italic">Accusantium illum cupiditate harum asperiores iusto quos quasi quis quae! Fugit doloribus, voluptatum quidem magnam velit excepturi nobis, reprehenderit ducimus incidunt quisquam quae veritatis, quos iure harum.</p>
+            <p className="px-6 py-1 text-lg italic">I love my new sofa! It's so comfortable and stylish, and it arrived in perfect condition. I would definitely recommend this company to anyone looking for new furniture.</p>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="absolute bottom-0 right-0 w-8 h-8 dark:text-gray-700">
               <path fill="currentColor" d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
               <path fill="currentColor" d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
             </svg>
           </div>
           <span className="w-12 h-1 my-2 rounded-lg dark:bg-violet-400"></span>
-          <p>Leroy Jenkins</p>
+          <p>Riyad Ahmed</p>
         </div>
       </div>
     </section>
